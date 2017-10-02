f9b2a6e569f7f2c843011751312451ea827b3b58
Elijah <elijahdj88@gmail.com>
2017-10-01 21:52:44 -0400

    lab 6 submission

---
+++ Lab06/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepGoing = true;
+
+            while (keepGoing == true)
+            {
+
+                Console.WriteLine("Welcome to the Pig Latin Translator!");
+
+                Console.WriteLine("Enter a line to be translated:");
+
+                string str = Console.ReadLine().ToLower();//reads user input and converts to all lowercase
+
+                if (str[0] == 'a' || str[0] == 'e' || str[0] == 'i' || str[0] == 'o' || str[0] == 'u')
+                //READS if user input starts with a vowel (a,e,i,o,u)
+                {
+                    Console.WriteLine(str + "way");
+
+                }
+                else
+                {
+                    char[] vow = { 'a', 'e', 'i', 'o', 'u' };
+                    int index = str.IndexOfAny(vow);
+                    string str1 = (str.Substring(0, index));
+                    string str2 = (str.Substring(index));
+                    Console.WriteLine(str2 + str1 + "ay");
+
+                }
+                string userChoice;
+                Console.WriteLine("Continue? (y/n)");
+                userChoice = Console.ReadLine();
+                if (userChoice == "n" || userChoice == "N")
+                {
+                    keepGoing = false;
+                }
+            }
+        }
+    }
+}
